86c8b1a32b77a6c203fbf91a68e644f6ee996bd8
dudulu <wack1001@163.com>
2020-03-14 15:03:40 +0800

    change setting values length

--- src/repository/types/setting.go
+++ src/repository/types/setting.go
@@ -3,8 +3,8 @@ package types
 import "time"
 
 type Setting struct {
-	Key   string `gorm:"column:key;index;unique;size:255" json:"key"`
-	Value string `gorm:"column:value;null;type:text;size:10000" json:"value"`
+	Key   string `gorm:"column:key;index;unique;size:168" json:"key"`
+	Value string `gorm:"column:value;null;type:text" json:"value"`
 	//Title       string    `gorm:"column:title;null" json:"title"`
 	Description string    `gorm:"column:description;null;size:500" json:"description"`
 	CreatedAt   time.Time `gorm:"column:created_at" json:"created_at"`
